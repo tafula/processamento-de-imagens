ace0c9820bf6d206af0a14e8468a29737234280b
Chris <tafula@autistici.org>
2016-03-30 20:36:07 -0300

    BlobDetector fixed

--- BlobDetector.cpp
+++ BlobDetector.cpp
@@ -4,70 +4,177 @@ BlobDetector::BlobDetector(int kernelShape, Size kernelSize, Point kernelAnchorP
   morphologicalKernel = getStructuringElement(kernelShape, kernelSize, kernelAnchorPoint);
   minAreaBlob = minArea;
   maxAreaBlob = maxArea;
-  backgroungSubtractor = createBackgroundSubtractorMOG2();
+
+#if labNote
+		backgroungSubtractor = createBackgroundSubtractorMOG2();
+#else
+		backgroungSubtractor = BackgroundSubtractorMOG2();
+#endif
+
   MIN_THRESHOLD = minT;
   DEST_THRESHOLD = maxT;
 }
 
+
+/*** Reseta Background ***/
+/**************************************************/
 void BlobDetector::resetBackground(){
+#if labNote
   backgroungSubtractor = createBackgroundSubtractorMOG2();
-}
+#else
+	backgroungSubtractor = BackgroundSubtractorMOG2();
+#endif
+} /************************************************/
 
+
+/*** Operacoes 'dilate' e 'erode' consecutivas preparando imagem para melhor tratamento ***/
+/**********************************************************/
 void BlobDetector::morphologyOperations(Mat orig, Mat dest){
-  //blur(orig, dest,Size(3,3));
-  blur(orig, dest, Size(5,5));
+	//blur(orig, dest,Size(3,3));
+	blur(orig, dest, Size(5,5));
 
-  dilate(orig, dest, morphologicalKernel);
-  erode(orig, dest, morphologicalKernel);
+	dilate(orig, dest, morphologicalKernel);
+	erode(orig, dest, morphologicalKernel);
 
-  erode(orig, dest, morphologicalKernel);
-  dilate(orig, dest, morphologicalKernel);
+	erode(orig, dest, morphologicalKernel);
+	dilate(orig, dest, morphologicalKernel);
   
-  dilate(orig,dest,morphologicalKernel);
-}
+	dilate(orig,dest,morphologicalKernel);
+} /*********************************************************/
 
+
+/*** Procura Blobs na imagem ***/
+/*************************************************************/
 Mat BlobDetector::findBlobs(Mat orig){
-  Mat foreground;
-  vector < vector<Point> > contours;
+	Mat foreground;
+	vector < vector<Point> > contours;
 
-  backgroungSubtractor->apply(orig.clone(), foreground, 0.0001); //DESCOBRIR O QUE E ESSE TERCEIRO PARAMETRO
+#if labNote
+	backgroungSubtractor->apply(orig.clone(), foreground, learnRate); //3o PARAMETRO: Learning rate do BackgroundSubtractor
+#else
+	backgroungSubtractor(orig.clone(), foreground, learnRate);
+#endif
 
-  threshold(foreground, foreground, MIN_THRESHOLD, DEST_THRESHOLD, CV_THRESH_BINARY);
+	threshold(foreground, foreground, MIN_THRESHOLD, DEST_THRESHOLD, CV_THRESH_BINARY);
 
-  findContours(foreground.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	findContours(foreground.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-  hulls.clear();
-  
-  for(int i = 0; i < contours.size(); i++){
-    vector<Point> hull;
-    convexHull( Mat(contours[i]), hull, false);
-    double area = contourArea(hull);
-    if(area > minAreaBlob && area < maxAreaBlob)
-      hulls.push_back(hull);
-  }
+	hulls.clear();
+	for(int i = 0; i < contours.size(); i++){
+		vector<Point> hull;
+		convexHull( Mat(contours[i]), hull, false);
+		double area = contourArea(hull);
+		if(area > minAreaBlob && area < maxAreaBlob)
+			hulls.push_back(hull);
+	}
 
-  return foreground;
-}
+	return foreground;
+} /***********************************************************/
 
+/*** Procura Ellipses na imagem ***/
+/*************************************************************/
+Mat BlobDetector::findEllipses(Mat orig){
+	Mat foreground;
+	vector < vector<Point> > contours;
+
+//	cvtColor(orig.clone(), orig, CV_BGR2GRAY);
+	GaussianBlur( orig, orig, Size( 9, 9 ), 2, 2 );
+
+#if labNote
+	backgroungSubtractor->apply(orig.clone(), foreground, learnRate); //3o PARAMETRO: Learning rate do BackgroundSubtractor
+#else
+	backgroungSubtractor(orig.clone(), foreground, learnRate);
+#endif
+
+	threshold(foreground, foreground, MIN_THRESHOLD, DEST_THRESHOLD, CV_THRESH_BINARY);
+
+	findContours(foreground.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	minEllipse.clear();
+	for( int i = 0; i < contours.size(); i++ ){
+		if( contours[i].size() > 20){
+			RotatedRect ellHull = fitEllipse( Mat(contours[i]) );
+			Size2f areaEll = ellHull.size;
+			double area = areaEll.width * areaEll.height;
+			if(area > minAreaBlob && area < maxAreaBlob){
+				if(areaEll.width/areaEll.height <= ECC_ELL && areaEll.height/areaEll.width <= ECC_ELL) minEllipse.push_back( ellHull );
+			}
+		}
+	}
+
+	return foreground;
+} /***********************************************************/
+
+/*** Aplica Hough Transform na imagem ***/
+/*************************************************************/
+Mat BlobDetector::findHough(Mat orig){	
+	houghBlobs.clear();
+	cvtColor(orig.clone(), orig, CV_BGR2GRAY);
+	GaussianBlur( orig, orig, Size(9, 9), 2, 2 );
+	HoughCircles( orig, houghBlobs, CV_HOUGH_GRADIENT, 1, orig.rows/16, 125, 30, 0, 0 );
+
+	return orig;
+} /***********************************************************/
+
+
+
+/*** Devolve lista de blobs ***/ 
+/***********************************************/
 vector< vector<Point> > BlobDetector::getBlobs(){
-  return hulls;
-}
+	return hulls;
+} /*********************************************/
 
+/*** Devolve lista de elipses ***/ 
+/***********************************************/
+vector< RotatedRect > BlobDetector::getEllipses(){
+	return minEllipse;
+} /*********************************************/
+
+/*** Devolve lista de hough circles ***/ 
+/***********************************************/
+vector< Vec3f > BlobDetector::getHough(){
+	return houghBlobs;
+} /*********************************************/
+
+
+/*** Desenha blobs identificados na imagem ***/
+/****************************************************/
 void BlobDetector::drawBlobs(Mat frame, Scalar color){
-  for(int i = 0; i < hulls.size(); i++){
-    drawContours(frame, hulls, i, color, 1, 8, vector<Vec4i>(), 0, Point());
-  }
-}
+	for(int i = 0; i < hulls.size(); i++){
+		drawContours(frame, hulls, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+	}
+} /**************************************************/
+
+/*** Desenha elipses identificadas na imagem ***/
+/****************************************************/
+void BlobDetector::drawEllipses(Mat frame, Scalar color){
+	for(int i = 0; i < minEllipse.size(); i++){
+		ellipse(frame, minEllipse[i], color, 2, 8 );
+	}
+} /**************************************************/
+
+/*** Desenha Hough Circles identificados na imagem ***/
+/****************************************************/
+void BlobDetector::drawHough(Mat frame, Scalar color){
+	for(int i = 0; i < houghBlobs.size(); i++){
+		Point center( cvRound(houghBlobs[i][0]), cvRound(houghBlobs[i][1]) );
+		int radius = cvRound(houghBlobs[i][2]);
+		circle( frame, center, radius, color, 2, 8, 0);
+	}
+} /**************************************************/
+
+		
+
 
 void BlobDetector::boundBlobs(Mat frame, Scalar color){
-  vector< vector<Point> >::const_iterator it = hulls.begin();
-  vector< vector<Point> >::const_iterator end = hulls.end();
+	vector< vector<Point> >::const_iterator it = hulls.begin();
+	vector< vector<Point> >::const_iterator end = hulls.end();
   
-  while(it != end){
-    Rect bounds = boundingRect(*it);
-    rectangle(frame, bounds, color, 2);
-    ++it;
-  }
+	while(it != end){
+		Rect bounds = boundingRect(*it);
+		rectangle(frame, bounds, color, 2);
+		++it;
+	}
 }
 
 
--- BlobDetector.hpp
+++ BlobDetector.hpp
@@ -1,6 +1,11 @@
 #ifndef BLOB_DETECTOR_H
 #define BLOB_DETECTOR_H
 
+#define labNote 0
+#define ECC_ELL 1.2
+
+#define learnRate 0 //Frequencia de atualizacao do BackgroundSubtractor(MOG2)
+
 #include<opencv2/opencv.hpp>
 
 using namespace std;
@@ -9,9 +14,15 @@ using namespace cv;
 
 class BlobDetector{
 private:
-  Ptr<BackgroundSubtractorMOG2> backgroungSubtractor;
+#if labNote
+	Ptr<BackgroundSubtractorMOG2> backgroungSubtractor;
+#else
+	BackgroundSubtractorMOG2 backgroungSubtractor;
+#endif
   Mat morphologicalKernel;
-  vector< vector<Point>  > hulls;
+  vector< vector<Point> > hulls;
+  vector< RotatedRect> minEllipse;
+  vector< Vec3f > houghBlobs;
   int minAreaBlob;
   int maxAreaBlob;
   int MIN_THRESHOLD;
@@ -21,10 +32,16 @@ private:
 public:
   BlobDetector(int kernelShape, Size kernelSize, Point kernelAnchorPoint, int minArea = 0, int maxArea = 1000000, int minT=240, int maxT=255);
   vector< vector<Point> > getBlobs();
+  vector< RotatedRect > getEllipses();
+  vector< Vec3f > getHough();
   void boundBlobs(Mat frame, Scalar color);
   void drawBlobs(Mat frame, Scalar color);
+  void drawEllipses(Mat frame, Scalar color);
+	void drawHough(Mat frame, Scalar color);
   void morphologyOperations(Mat orig, Mat dest);
   Mat findBlobs(Mat orig);
+  Mat findEllipses(Mat orig);
+  Mat findHough(Mat orig);
   void resetBackground();
 };
 
